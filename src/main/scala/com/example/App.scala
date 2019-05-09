c8565213ed049af1c82f5e21984730640769b3fe
Vakhtang Laluashvili <v.laluashvili@sda.gov.ge>
2019-05-09 17:06:20 +0400

    Added configuration option.

--- src/main/scala/com/example/App.scala
+++ src/main/scala/com/example/App.scala
@@ -3,22 +3,30 @@ package com.example
 import org.apache.hadoop.fs.{FileSystem, Path}
 import org.apache.spark.sql.SparkSession
 
-import scala.reflect.io.File
-
 object App {
 
   def main(args: Array[String]) {
 
-    if (args.length < 2)
+    // Create (or get) spark session
+    val sc = SparkSession.builder
+      .appName("Simple Application")
+      .getOrCreate
+
+    // Get configuration from context
+    val conf = sc.sparkContext.getConf
+
+    // Read inputPath and outputPath strings from configuration file
+    val inputPath = conf.get("inputPath")
+    val outputPath = conf.get("outputPath")
+
+    if (args.length < 2
+      || inputPath.isEmpty
+      || outputPath.isEmpty)
       System.err.println("You should pass two arguments: input and output paths!")
     else {
-      // Create (or get) spark session
-      val sc = SparkSession.builder
-        .appName("Simple Application")
-        .getOrCreate
 
       // First argument is the input
-      val textFile = sc.read.textFile(args(0))
+      val textFile = sc.read.textFile(inputPath)
 
       // Count work occurrences...
       val counts = textFile.rdd
@@ -28,16 +36,17 @@ object App {
 
       // Delete old output path
       val fs = FileSystem.get(sc.sparkContext.hadoopConfiguration)
-      val outputPath = new Path(args(1))
-      if (fs.exists(outputPath))
-        fs.delete(outputPath, true)
+      val output = new Path(outputPath)
+      if (fs.exists(output))
+        fs.delete(output, true)
 
       // Save result as text file
-      counts.saveAsTextFile(args(1))
+      counts.saveAsTextFile(outputPath)
 
-      // Closing the spark session
-      sc.close
     }
+
+    // Closing the spark session
+    sc.close
   }
 
 }
